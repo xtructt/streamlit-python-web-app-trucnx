5e129ea5a30468b3ae53133ffb9ce38b12e19150
xtructt <xtruc.tt@gmail.com>
2020-08-13 16:37:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ import pandas as pd
 import numpy as np
 from dateutil.parser import parse
 
-time_series_covid19_confirmed_US = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+
+
 time_series_covid19_confirmed_global= 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+time_series_covid19_confirmed_US = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
 time_series_covid19_deaths_US = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
 time_series_covid19_deaths_global = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
 time_series_covid19_recovered_global = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
-
-
+#Create DF from csv files
 confirmed_US = pd.read_csv(time_series_covid19_confirmed_US)
 confirmed_global = pd.read_csv(time_series_covid19_confirmed_global)
 deaths_US = pd.read_csv(time_series_covid19_deaths_US)
 deaths_global = pd.read_csv(time_series_covid19_deaths_global)
 recovered_global = pd.read_csv(time_series_covid19_recovered_global)
+
 
 #Melt date columns and it values of each data frame
 def is_date(string, fuzzy=False):
@@ -41,14 +43,26 @@ def find_indexes_columns (table_df):
   return index_cols
 
 
-confirmed_US_melted = pd.melt(confirmed_US,id_vars=find_indexes_columns(confirmed_US), var_name = "Date", value_name="Confirmed_cases")
-confirmed_global_melted = pd.melt(confirmed_global,id_vars=find_indexes_columns(confirmed_global), var_name = "Date", value_name="Confirmed_cases")
-deaths_US_melted = pd.melt(deaths_US,id_vars=find_indexes_columns(deaths_US), var_name="Date", value_name="number_of_deaths")
-deaths_global_melted = pd.melt(deaths_global,id_vars=find_indexes_columns(deaths_global), var_name="Date", value_name="number_of_deaths")
-recovered_global = pd.melt(recovered_global, id_vars=find_indexes_columns(recovered_global), var_name="Date", value_name="number_of_deaths")
+#confirmed_global_melted = pd.melt(confirmed_global,id_vars=find_indexes_columns(confirmed_global), var_name = "Date", value_name="Confirmed_cases")
+#deaths_US_melted = pd.melt(deaths_US,id_vars=find_indexes_columns(deaths_US), var_name="Date", value_name="number_of_deaths")
+#deaths_global_melted = pd.melt(deaths_global,id_vars=find_indexes_columns(deaths_global), var_name="Date", value_name="number_of_deaths")
+#recovered_global = pd.melt(recovered_global, id_vars=find_indexes_columns(recovered_global), var_name="Date", value_name="number_of_deaths")
 
-st.write(confirmed_US_melted.head(100))
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'Global or US?',
+    ('Global', 'US')
+)
 
 
-
-
+if add_selectbox == 'US':
+  confirmed_US_melted = pd.melt(confirmed_US,id_vars=find_indexes_columns(confirmed_US), var_name = "Date", value_name="Confirmed_cases")
+  st.write(confirmed_US_melted.head())
+elif add_selectbox == 'Global':
+  country_list = confirmed_global['Country/Region'].unique()
+  add_country = st.sidebar.selectbox(
+    'Choose the country',
+    country_list
+  )
+  confirmed_global_melted = pd.melt(confirmed_global[confirmed_global['Country/Region']== add_country],id_vars=find_indexes_columns(confirmed_global), var_name = "Date", value_name="Confirmed_cases")
+  st.dataframe(confirmed_global_melted.head())
